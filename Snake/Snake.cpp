d537cb86aabbd833b3a9abe184904705481943c1
Veezeer <wiktormachon@o2.pl>
2021-06-06 11:12:13 +0200

    Added Button.h, Game States, Main Menu

--- Snake/Snake.cpp
+++ Snake/Snake.cpp
@@ -1,4 +1,5 @@
-﻿//Biblioteki
+﻿#pragma warning(disable : 4996) 
+//Biblioteki
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <windows.h>
@@ -6,14 +7,16 @@
 //Klasy
 #include "Player.h"
 #include "Collectible.h"
-
+#include "Button.h"
 //using namespace sf;
 
 /*Wymiary okna*/
 const int WindowWidth = 720;
 const int WindowHeight = 720;
 
-
+/*Stany w których może być gra*/
+enum STATES { MAIN_MENU = 1, GAMEPLAY, GAME_OVER, HOW_TO_PLAY, BEST_SCORES, PAUSE };
+char GAME_STATE = STATES::MAIN_MENU;
 
 //Okreslenie kierunku
 /*
@@ -41,8 +44,38 @@ int main(){
     Player player(WindowWidth/2, WindowHeight/2);
     Collectible coll;
 
+    //FONT
+    sf::Font font;
+    font.loadFromFile("Fonts/dogica.ttf");
 
-    while (window.isOpen()){
+    /*MAIN MENU SETUP*/
+    sf::Sprite MainMenu;
+    sf::Texture MainMenuBackground;
+    sf::Text authorText;
+
+    Button startGameButton(187, 173);
+    startGameButton.setTextTexture("Graphics/grajtxt.png");
+    Button bestScoresButton(187, 253);
+    bestScoresButton.setTextTexture("Graphics/bestscorestxt.png");
+    Button howToButton(187, 333);
+    howToButton.setTextTexture("Graphics/howtotxt.png");
+    Button exitGameButton(187, 413);
+    exitGameButton.setTextTexture("Graphics/wyjdztxt.png");
+
+    authorText.setFont(font);
+    authorText.setColor({ 86, 27, 174 });
+    authorText.setCharacterSize(14);
+    authorText.setOrigin({ 170,20 });
+    authorText.setPosition({ WindowWidth - 220, WindowHeight - 5 });
+    authorText.setString("Author: PK/WM");
+
+    if (!MainMenuBackground.loadFromFile("Graphics/mainmenu.png")) {
+        std::cout << "Error: Texture not found" << std::endl;
+    }
+    MainMenu.setTexture(MainMenuBackground);
+
+    while (window.isOpen())
+    {
         window.clear();
         sf::Event event;
         while (window.pollEvent(event)){
@@ -55,8 +88,103 @@ int main(){
                 else if ((event.key.code == sf::Keyboard::A || event.key.code == sf::Keyboard::Left) && dir != 2) dir = 3;
             }
         }
+        switch (GAME_STATE) {
+        case MAIN_MENU:
+            /*-----MAIN MENU-----*/
 
-        for (int i = size; i > 0; i--) {
+            if (startGameButton.isPressed(window)) {
+                GAME_STATE = STATES::GAMEPLAY;
+            }
+            else if (bestScoresButton.isPressed(window)) {
+                GAME_STATE = STATES::BEST_SCORES;
+            }
+            else if (howToButton.isPressed(window)) {
+                GAME_STATE = STATES::HOW_TO_PLAY;
+            }
+            else if (exitGameButton.isPressed(window)) {
+                window.close();
+            }
+
+            window.draw(MainMenu);
+            window.draw(authorText);
+            window.draw(startGameButton);
+            window.draw(bestScoresButton);
+            window.draw(howToButton);
+            window.draw(exitGameButton);
+            /*----/MAIN MENU-----*/
+            break;
+
+        case GAMEPLAY:
+            /*-----GAMEPLAY-----*/
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Escape)) {
+                GAME_STATE = STATES::PAUSE;
+            }
+
+            for (int i = size; i > 0; i--) {
+                p[i].x = p[i - 1].x;
+                p[i].y = p[i - 1].y;
+            }
+
+            if (dir == 0) {
+                if (p[0].y > 0) p[0].y -= step;
+                else p[0].y += WindowHeight - step;
+            }
+
+            if (dir == 1) {
+                if (p[0].y < WindowHeight - step) p[0].y += step;
+                else p[0].y -= WindowHeight - step;
+            }
+            if (dir == 2) {
+                if (p[0].x < WindowWidth - step) p[0].x += step;
+                else p[0].x -= WindowWidth - step;
+            }
+            if (dir == 3) {
+                if (p[0].x > 0) p[0].x -= step;
+                else p[0].x += WindowWidth - step;
+            }
+
+
+            if (p[0].x == coll.getPosition().x && p[0].y == coll.getPosition().y) {
+                size++;
+                coll.randomPosition();
+            }
+
+            for (int i = 0; i < size; i++) {
+                player.setPosition(p[i].x, p[i].y);
+                window.draw(player);
+            }
+                window.draw(coll);
+
+            /*-----/GAMEPLAY-----*/;
+            break;
+                  
+        case GAME_OVER:
+            /*-----GAME OVER-----*/
+            
+            /*-----/GAME OVER-----*/
+            break;
+
+        case BEST_SCORES:
+            /*------BEST SCORES-----*/
+           
+            /*------/BEST SCORES-----*/
+            break;
+
+            /*------HOW TO PLAY-----*/
+        case HOW_TO_PLAY:
+            
+            /*------/HOW TO PLAY-----*/
+            break;
+
+            /*------PAUSE-----*/
+        case PAUSE:
+            
+            /*------/PAUSE-----*/
+            break;
+        }
+
+
+      /* for (int i = size; i > 0; i--) {
             p[i].x = p[i - 1].x;
             p[i].y = p[i - 1].y;
         }
@@ -88,8 +216,8 @@ int main(){
         for (int i = 0; i < size; i++) {
             player.setPosition(p[i].x, p[i].y);
             window.draw(player);
-        }
-        window.draw(coll);
+        }*/
+        //window.draw(coll);
       
         window.display(); 
         Sleep(speed);
